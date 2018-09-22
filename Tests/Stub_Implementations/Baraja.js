ba1068b233ae9d2033beeeb5d7355d4c773ba9d6
Daniel Maldonado <daniel.maldonado@mercadolibre.com>
2018-09-22 16:31:32 -0300

    real stub

--- Tests/Stub_Implementations/Baraja.js
+++ Tests/Stub_Implementations/Baraja.js
@@ -1,20 +1,7 @@
-const Pozo = require('../../Domain/Pozo')
-const TotalCartas = require('../../Domain/TotalCartas')
+const Baraja = require('../../Domain/Baraja')
 
 module.exports = (...cartas) => {
-    return {
-        cartaCima() {
-            const carta = cartas.shift()
-            if (!carta) {
-                throw new Error('Baraja vacia')
-            }
-            return carta
-        },
-        iniciarPozo(pozo) {
-            return pozo.nuevo(this.cartaCima())
-        },
-        totalCartas() {
-            return TotalCartas(cartas.length)
-        }
-    }
+    const baraja = Baraja()
+    baraja.cartaCima = () => cartas.shift()
+    return baraja
 }
